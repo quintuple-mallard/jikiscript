44361d694b47640b9ae8342eefb3c98559430c4d
J R M <jwrmmine@protonmail.com>
2025-08-10 20:55:55 +0200

    Make cli js based

--- run.js
+++ run.js
@@ -1,12 +1,38 @@
-function makeEnum(...args) {}
-function runFile(filepath) {
-  console.log(`path: ${filepath}`);
+class JikiScript {
+  static runFile(filepath) {
+    // Do something with the file
+  }
+  static runLine(line) {
+    console.log(line)
+  }
+  static interactiveMode() {
+    const readline = require("readline");
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    const promptUser = () => {
+      rl.question(">>> ", (line) => {
+        if (!line) {
+          rl.close(); 
+          return;
+        }
+        JikiScript.runLine(line); 
+        promptUser(); 
+      });
+    };
+
+    promptUser();
+  }
+
+  static cli() {
+    if (process.argv.length > 3) {
+      console.log("Usage: jiki [script]");
+    } else if (process.argv[2]) JikiScript.runFile(process.argv[2]);
+    else {
+      JikiScript.interactiveMode();
+    }
+  }
 }
-function runLine(line) {
-  console.log(`line: ${line}`);
-}
-if (process.argv[3] === "file") {
-  runFile(process.argv[2]);
-} else {
-  runLine(process.argv[2]);
-}
+JikiScript.cli();
