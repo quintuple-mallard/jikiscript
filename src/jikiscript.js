624cd32ce55812f1e53ece64f16449a8794656f8
J R M <jwrmmine@protonmail.com>
2025-08-14 19:47:17 +0200

    tweak error message

--- src/jikiscript.js
+++ src/jikiscript.js
@@ -53,7 +53,7 @@ class JikiScript {
 
     while (current < jiki.length) {
       let char = jiki[current];
-      throw new Error(`Unknown character ${char}`);
+      throw new Error(`Unknown character "${char}"`);
     }
     return tokens;
   }
