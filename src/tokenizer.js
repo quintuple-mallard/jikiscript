896a96790858318b446e1b121240f81a373901e6
J R M <jwrmmine@protonmail.com>
2025-08-15 08:49:25 +0200

    Update tokenizer.js

--- src/tokenizer.js
+++ src/tokenizer.js
@@ -1,3 +1,11 @@
-module.exports = function tokenizer(jikiscript) {
-  
-};
+module.exports = function tokenize(jiki) {
+  const tokens = [];
+  let current = 0;
+
+  while (current < jiki.length) {
+    let char = jiki[current];
+    throw new Error(`Unknown character "${char}"`);
+    //process.exit(1)
+  }
+  return tokens;
+}
